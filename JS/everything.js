d821df9a00265d3ae1647049ceeda33c2baa3a86
Igor Chernega <am.igor.chernega@gmail.com>
2017-08-04 20:10:38 +0300

    Front end basics for managing issues in work
    Now you can click a button and an issue disappears from
    search list to appear it the issues in work list.

--- JS/everything.js
+++ JS/everything.js
@@ -1,3 +1,5 @@
+var items;
+
 function requestJSON(url, callback) {
     $.ajax({
       url: url,
@@ -17,31 +19,44 @@ $('#main-search').on('click', function(e){
         } else {
             var resultsNumber = json.total_count;
             var outHTML = '<h2>' + resultsNumber + ' issues</h2>';
-            var items = json.items;
+            items = json.items;
             items.forEach(function(item, i, items) {
-                outHTML += addSearchResultItem(item);
+                outHTML += addSearchResultItem(item, i);
             });
             $('#search-results').html(outHTML);
             console.log(json);
+            $('.in-work').on('click', function(e) {
+                e.preventDefault();
+                var itemPosition = getNumberFromString($(this).parent().attr('id'));
+                console.log("IP="+itemPosition);
+                $(this).parent().hide();
+                addIssueInWork(itemPosition);
+            });
         } //end else statement
     } // end requestJSON function
 ); // end click event handler
 });
 });
 
-function addSearchResultItem(item) {
+function addSearchResultItem(item, position) {
     var outHTML = '';
     var title = '<div class="item-title"><h4><a href="' + item.html_url + '">' + item.title + '</a></h4></div>';
     var descr = '<div class="item-descr">' + item.body + '</div>';
     var number = '<div class="item-number">#' + item.number + '</div>';
-    var inWorkButton = '<button type="button" class="in-work btn btn-info">In Work</button>'
+    var inWorkButton = '<button type="button" class="in-work btn btn-primary">In Work</button>'
     outHTML = number + title + descr + inWorkButton;
-    outHTML = '<li>' + outHTML + '</li>';
+    outHTML = '<li id="item-' + position + '">' + outHTML + '</li>';
     return outHTML;
 }
 
+function getNumberFromString(str) {
+    var numberPos = str.indexOf(/^\d+/);
+    var number = str.slice(numberPos);
+    return number;
+}
 
-$('.in-work').on('click', function(e) {
-    e.preventDefault();
-    alert("!");
-});
+function addIssueInWork(position) {
+    var issuesInWork = $('#issue-in-work').find('ul').html();
+    issuesInWork += '<li><a href="' + items[position].html_url + '">' + items[position].title + '</a></li>';
+    $('#issue-in-work').find('ul').html(issuesInWork);
+}
