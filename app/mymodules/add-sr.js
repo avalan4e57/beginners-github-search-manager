1bc5ac928a21128787408c107e6862afd674259e
Igor Chernega <am.igor.chernega@gmail.com>
2017-08-25 16:56:30 +0300

    Get next page with links as param

--- app/mymodules/add-sr.js
+++ app/mymodules/add-sr.js
@@ -1,4 +1,4 @@
-exports.add = (query, callback) => {
+exports.add = (cur, last, callback) => {
     var path = require('path')
     var fs = require('fs')
     var GitHubApi = require("github")
@@ -12,39 +12,90 @@ exports.add = (query, callback) => {
 
 
     var searchresults = []
-    github.search.issues({ q: query }, (err, data) => {
-        // console.log(data)
-        for (let issue of data.data.items) {
-            searchresults.push({
-                "url": issue.html_url,
-                'title': issue.title,
-                'body': issue.body,
-                'number': issue.number
-            })
-        }
+    var curLink = []
+    var url = 'https://api.github.com/search/issues?q=language:JavaScript+is:up-for-grabs+state:open'
+    var result = {}
 
-        fs.readFile(__dirname + '/../managerdata.json', (err, data) => {
-            if (err) return console.error(err)
-            let managerdata = JSON.parse(data)
-            for (let stored of managerdata.inwork) {
-                searchresults = searchresults.filter((item) => {
-                    return item.url !== stored.url
+    if (cur == 1) {
+        curLink = [
+            getLink("next", url, cur, last),
+            getLink("last", url, cur, last)
+        ]
+        result.totalPages = curLink[1]
+    } else {
+        curLink = [
+            getLink("next", url, cur, last),
+            getLink("last", url, cur, last),
+            getLink("first", url, cur, last),
+            getLink("prev", url, cur, last)
+        ]
+    }
+
+    let queryLink = curLink.join(", ")
+    console.log(queryLink)
+    if (github.hasNextPage(queryLink)) {
+        github.getNextPage(queryLink, (err, data) => {
+            console.log(data)
+            for (let issue of data.data.items) {
+                searchresults.push({
+                    "url": issue.html_url,
+                    'title': issue.title,
+                    'body': issue.body,
+                    'number': issue.number
                 })
             }
-            for (let deleted of managerdata.deleted) {
-                searchresults = searchresults.filter((item) => {
-                    return item.url !== deleted
-                })
-            }
-            for (let stored of managerdata.pred) {
-                searchresults = searchresults.filter((item) => {
-                    return item.url !== stored.url
-                })
-            }
-            let rend = {
-                "issues": searchresults
-            }
-            callback(rend)
-        })//end readFile
-    })//end github.search.issues
+
+            fs.readFile(__dirname + '/../managerdata.json', (err, data) => {
+                if (err) return console.error(err)
+                let managerdata = JSON.parse(data)
+                for (let stored of managerdata.inwork) {
+                    searchresults = searchresults.filter((item) => {
+                        return item.url !== stored.url
+                    })
+                }
+                for (let deleted of managerdata.deleted) {
+                    searchresults = searchresults.filter((item) => {
+                        return item.url !== deleted
+                    })
+                }
+                for (let stored of managerdata.pred) {
+                    searchresults = searchresults.filter((item) => {
+                        return item.url !== stored.url
+                    })
+                }
+                // let rend = {
+                //     "issues": searchresults,
+                //     "totalPages": totalPages
+                // }
+                result.issues = searchresults
+                callback(result)
+            })//end readFile
+        })//end github.search.issues
+    } else {
+        result.issues = []
+        callback(result)
+    }
+
+}
+
+function getLink(name, url, curPage, lastPage) {
+    curPage = Number(curPage)
+    var link = ""
+    switch (name) {
+        case "next":
+            link += '<' + url + '&page=' + (curPage + 1).toString() + '>; rel="next"'
+            break;
+        case "prev":
+            link += '<' + url + '&page=' + (curPage - 1).toString() + '>; rel="prev"'
+            break;
+        case "first":
+            link += '<' + url + '&page=1>; rel="first"'
+            break;
+        case "last":
+            link += '<' + url + '&page=' + lastPage + '>; rel="last"'
+            break;
+        default:
+            link = null
+    }
+    return link
 }
