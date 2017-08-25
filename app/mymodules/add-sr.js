ddd348541ec4a684092dc703c9642bbdcea0bd23
Igor Chernega <am.igor.chernega@gmail.com>
2017-08-25 18:08:16 +0300

    Refactored add-sr.js so it works fine and gives valid page results

--- app/mymodules/add-sr.js
+++ app/mymodules/add-sr.js
@@ -10,8 +10,6 @@ exports.add = (cur, last, callback) => {
         token: "b1881cd13b64b30d7ccbb40a50d2a4a0f52089eb",
     })
 
-
-    var searchresults = []
     var curLink = []
     var url = 'https://api.github.com/search/issues?q=language:JavaScript+is:up-for-grabs+state:open'
     var result = {}
@@ -19,9 +17,19 @@ exports.add = (cur, last, callback) => {
     if (cur == 1) {
         curLink = [
             getLink("next", url, cur, last),
-            getLink("last", url, cur, last)
+            getLink("last", url, cur, last),
+            getLink("first", url, cur, last)
         ]
         result.totalPages = curLink[1]
+
+        let queryLink = curLink.join(", ")
+        console.log(queryLink)
+        github.getFirstPage(queryLink, (err, data) => {
+            main(data, searchresults => {
+                result.issues = searchresults
+                callback(result)
+            })
+        })//end github.search.issues
     } else {
         curLink = [
             getLink("next", url, cur, last),
@@ -29,73 +37,83 @@ exports.add = (cur, last, callback) => {
             getLink("first", url, cur, last),
             getLink("prev", url, cur, last)
         ]
+
+        let queryLink = curLink.join(", ")
+        console.log(queryLink)
+        if (github.hasNextPage(queryLink)) {
+            github.getNextPage(queryLink, (err, data) => {
+                main(data, searchresults => {
+                    result.issues = searchresults
+                    callback(result)
+                })
+
+            })//end github.search.issues
+        } else {
+            result.issues = []
+            callback(result)
+        }
     }
 
-    let queryLink = curLink.join(", ")
-    console.log(queryLink)
-    if (github.hasNextPage(queryLink)) {
-        github.getNextPage(queryLink, (err, data) => {
-            console.log(data)
-            for (let issue of data.data.items) {
-                searchresults.push({
-                    "url": issue.html_url,
-                    'title': issue.title,
-                    'body': issue.body,
-                    'number': issue.number
+
+
+    ///////////////////////////
+    function getLink(name, url, curPage, lastPage) {
+        curPage = Number(curPage)
+        var link = ""
+        switch (name) {
+            case "next":
+                link += '<' + url + '&page=' + (curPage + 1).toString() + '>; rel="next"'
+                break;
+            case "prev":
+                link += '<' + url + '&page=' + (curPage - 1).toString() + '>; rel="prev"'
+                break;
+            case "first":
+                link += '<' + url + '&page=1>; rel="first"'
+                break;
+            case "last":
+                link += '<' + url + '&page=' + lastPage + '>; rel="last"'
+                break;
+            default:
+                link = null
+        }
+        return link
+    }
+
+    function main(data, callback) {
+        var searchresults = []
+        console.log(data)
+        for (let issue of data.data.items) {
+            searchresults.push({
+                "url": issue.html_url,
+                'title': issue.title,
+                'body': issue.body,
+                'number': issue.number
+            })
+        }
+
+        fs.readFile(__dirname + '/../managerdata.json', (err, data) => {
+            if (err) return console.error(err)
+            let managerdata = JSON.parse(data)
+            for (let stored of managerdata.inwork) {
+                searchresults = searchresults.filter((item) => {
+                    return item.url !== stored.url
                 })
             }
-
-            fs.readFile(__dirname + '/../managerdata.json', (err, data) => {
-                if (err) return console.error(err)
-                let managerdata = JSON.parse(data)
-                for (let stored of managerdata.inwork) {
-                    searchresults = searchresults.filter((item) => {
-                        return item.url !== stored.url
-                    })
-                }
-                for (let deleted of managerdata.deleted) {
-                    searchresults = searchresults.filter((item) => {
-                        return item.url !== deleted
-                    })
-                }
-                for (let stored of managerdata.pred) {
-                    searchresults = searchresults.filter((item) => {
-                        return item.url !== stored.url
-                    })
-                }
-                // let rend = {
-                //     "issues": searchresults,
-                //     "totalPages": totalPages
-                // }
-                result.issues = searchresults
-                callback(result)
-            })//end readFile
-        })//end github.search.issues
-    } else {
-        result.issues = []
-        callback(result)
+            for (let deleted of managerdata.deleted) {
+                searchresults = searchresults.filter((item) => {
+                    return item.url !== deleted
+                })
+            }
+            for (let stored of managerdata.pred) {
+                searchresults = searchresults.filter((item) => {
+                    return item.url !== stored.url
+                })
+            }
+            // let rend = {
+            //     "issues": searchresults,
+            //     "totalPages": totalPages
+            // }
+            callback(searchresults)
+        })//end readFile
     }
-
-}
-
-function getLink(name, url, curPage, lastPage) {
-    curPage = Number(curPage)
-    var link = ""
-    switch (name) {
-        case "next":
-            link += '<' + url + '&page=' + (curPage + 1).toString() + '>; rel="next"'
-            break;
-        case "prev":
-            link += '<' + url + '&page=' + (curPage - 1).toString() + '>; rel="prev"'
-            break;
-        case "first":
-            link += '<' + url + '&page=1>; rel="first"'
-            break;
-        case "last":
-            link += '<' + url + '&page=' + lastPage + '>; rel="last"'
-            break;
-        default:
-            link = null
-    }
-    return link
 }
